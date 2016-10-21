085f91c3a7394d3f6727d1c623589e660b4c4e12
JaroslavDavidek <JaroslavDavidek@gmail.com>
2016-10-21 22:50:51 +0200

    Created missing repositories, added IEntity implementation to CostInfo.

---
+++ PV247/BL/Repositories/UserBadgeRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFramework;
+
+namespace BL.Repositories
+{
+    public class UserBadgeRepository : EntityFrameworkRepository<UserBadge, int>
+    {
+        public UserBadgeRepository(IUnitOfWorkProvider provider) : base(provider) { }
+    }
+}
