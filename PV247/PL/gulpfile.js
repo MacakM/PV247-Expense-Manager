e5e9b0a09afcddd37bae8d2eab6e4b1588354b2d
mayo505 <marek.turis@gmail.com>
2016-10-27 13:30:53 +0200

    Added basic gulpfile

---
+++ PV247/PL/gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var watch = require('gulp-watch');
+
+gulp.task('default', function () {
+    convertLessToCss();
+});
+
+gulp.task('less', convertLessToCss);
+
+gulp.task('watch', function() {
+    gulp.watch('./wwwroot/less/**/*.less', convertLessToCss);
+});
+
+function convertLessToCss() {
+    return gulp.src('./wwwroot/less/**/*.less')
+      .pipe(less({
+          paths: [path.join(__dirname, 'less', 'includes')]
+      }))
+      .pipe(gulp.dest('./wwwroot/css'));
+}
