4156981f70ef3cb68bd18a33c3ad2ecc14a4d6df
mayo505 <marek.turis@gmail.com>
2016-10-28 11:49:40 +0200

    Added main page layout, for now just static

---
+++ PV247/PL/wwwroot/js/main.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    var menuOpened = false;
+
+    $(".datepicker").datepicker();
+
+    $("#left-menu-opener").click(function () {
+        var padding;
+        var margin;
+        if (menuOpened) {
+            padding = 0;
+            margin = -202;
+            menuOpened = false;
+        } else {
+            padding = 200;
+            margin = -2; // there where problems if this was set to 0
+            menuOpened = true;
+        }
+
+        $(".container-fluid")
+            .animate({
+                'padding-left': padding
+            }, 500);
+        
+        $("#left-menu")
+            .animate({
+                'margin-left': margin
+            }, 500);
+    });
+
+    //$("#left-menu-opener").click(function () {
+    //    $(".container-fluid").toggleClass("menu-opened", 1000);
+    //    $("#left-menu").toggleClass("menu-opened", 1000);
+    //});
+});
