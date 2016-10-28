7a3023ca17cec22611317c10ee849c1ee68606d8
MacakM <410452@mail.muni.cz>
2016-10-28 20:26:17 +0200

    Fixed minor bugs in logging

--- PV247/IdentityDAL/IdentityDbContext.cs
+++ PV247/IdentityDAL/IdentityDbContext.cs
@@ -6,6 +6,15 @@ namespace IdentityDAL
 {
     public class IdentityDbContext : IdentityDbContext<ApplicationUser>
     {
+        public IdentityDbContext() { }
         public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Connection string is required by SQL server
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=IdentityStoreDB;Integrated Security=True;MultipleActiveResultSets=true");
+        }
     }
 }
