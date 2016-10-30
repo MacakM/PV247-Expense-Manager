dc3fa226ab3a519663c26ab13954cd86736d7f45
mayo505 <marek.turis@gmail.com>
2016-10-30 18:47:17 +0100

    Implemented basic expense front-end

---
+++ PV247/ExpenseManager.Presentation/Controllers/ExpenseController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManager.Presentation.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
