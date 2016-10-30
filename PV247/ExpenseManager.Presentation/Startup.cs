91f3961b568dcc689e92fb0de365503501c3cfd0
mayo505 <marek.turis@gmail.com>
2016-10-30 18:49:01 +0100

    Changed default root route

--- PV247/ExpenseManager.Presentation/Startup.cs
+++ PV247/ExpenseManager.Presentation/Startup.cs
@@ -93,7 +93,7 @@ namespace ExpenseManager.Presentation
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Expense}/{action=Index}/{id?}");
             });
         }
 
