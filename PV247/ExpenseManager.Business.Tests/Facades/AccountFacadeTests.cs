16d89c8e1abf2aad55807ab0e226db22e6bce530
xgasior <o.gasior@hotmail.com>
2016-11-07 01:39:53 +0100

    Tests stub

--- PV247/ExpenseManager.Business.Tests/Facades/AccountFacadeTests.cs
+++ PV247/ExpenseManager.Business.Tests/Facades/AccountFacadeTests.cs
@@ -1,6 +1,64 @@
-﻿namespace ExpenseManager.Business.Tests.Facades
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpenseManager.Business.Tests.Facades
 {
+    [TestClass]
     public class AccountFacadeTests
     {
+        [TestMethod]
+        public void RegisterNewUserTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateUserTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetCurrentlySignedUserTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteUserTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetUserTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListUsersTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreateAccountTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteAccountTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateAccountTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetAccountTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListAccountsTest()
+        {
+            throw new AssertFailedException();
+        }
     }
 }
--- PV247/ExpenseManager.Business.Tests/Facades/BalanceFacadeTests.cs
+++ PV247/ExpenseManager.Business.Tests/Facades/BalanceFacadeTests.cs
@@ -1,6 +1,151 @@
-﻿namespace ExpenseManager.Business.Tests.Facades
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpenseManager.Business.Tests.Facades
 {
+    [TestClass]
     public class BalanceFacadeTests
     {
+        [TestMethod]
+        public void ListAllCloseablePlans()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CheckBadgesRequirements()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void RecomputePeriodicCosts()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreateItemTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteItemTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateItemTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetItemTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListItemTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreatePlanTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeletePlanTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdatePlanTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetPlanTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListPlansTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreateItemTypeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteItemTypeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateItemTypeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetItemTypeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListItemTypesTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreateBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void GetBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListBagesTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void CreateAccountBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void DeleteAccountBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void UpdateAccountBadgeTest()
+        {
+            throw new AssertFailedException();
+
+        }
+        [TestMethod]
+        public void GetAccountBadgeTest()
+        {
+            throw new AssertFailedException();
+        }
+        [TestMethod]
+        public void ListAccountBadgesTest()
+        {
+           throw new AssertFailedException();
+        }
+    
     }
 }
