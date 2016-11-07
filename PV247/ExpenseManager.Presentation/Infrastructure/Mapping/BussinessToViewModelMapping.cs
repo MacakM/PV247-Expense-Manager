d029879dc25ecaeb148850676bd5eb2db0d025d5
mayo505 <marek.turis@gmail.com>
2016-11-07 22:08:16 +0100

    Created mapper configuration for PL

---
+++ PV247/ExpenseManager.Presentation/Infrastructure/Mapping/BussinessToViewModelMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ExpenseManager.Business.DataTransferObjects;
+using ExpenseManager.Presentation.Models.Expense;
+
+namespace ExpenseManager.Presentation.Infrastructure.Mapping
+{
+    class BussinessToViewModelMapping : Profile
+    {
+        public BussinessToViewModelMapping()
+        {
+            CreateMap<CostInfo, IndexViewModel>()
+                .ReverseMap();
+        }
+    }
+}
