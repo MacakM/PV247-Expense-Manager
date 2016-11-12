aa97b4d2c886a3d2ce48101418aeb56c179bbbf2
mayo505 <marek.turis@gmail.com>
2016-11-12 22:23:26 +0100

    Controller for displaying errors created

---
+++ PV247/ExpenseManager.Presentation/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManager.Presentation.Controllers
+{
+    /// <summary>
+    /// Controller for handling and displaying errors
+    /// </summary>
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// Displays error message
+        /// </summary>
+        public IActionResult Index()
+        {
+            var message = TempData["ErrorMessage"] ?? "Unknown error occured";
+
+            return View(message);
+        }
+    }
+}
