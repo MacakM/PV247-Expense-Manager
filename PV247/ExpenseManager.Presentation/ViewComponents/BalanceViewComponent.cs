649eb21b3dac1d5a5c474c56bfe30ce92d3cc160
mayo505 <marek.turis@gmail.com>
2016-11-24 18:42:58 +0100

    Current balance displayed

---
+++ PV247/ExpenseManager.Presentation/ViewComponents/BalanceViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseManager.Business.Facades;
+using ExpenseManager.Presentation.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManager.Presentation.ViewComponents
+{
+    /// <summary>
+    /// Component for displaying current balance
+    /// </summary>
+    public class BalanceViewComponent : ViewComponent
+    {
+        private readonly BalanceFacade _balanceFacade;
+        private readonly ICurrentAccountProvider _currentAccountProvider;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="balanceFacade"></param>
+        /// <param name="currentAccountProvider"></param>
+        public BalanceViewComponent(BalanceFacade balanceFacade, ICurrentAccountProvider currentAccountProvider)
+        {
+            _balanceFacade = balanceFacade;
+            _currentAccountProvider = currentAccountProvider;
+        }
+
+        /// <summary>
+        /// Invokes view component
+        /// </summary>
+        /// <returns></returns>
+        public IViewComponentResult Invoke()
+        {
+            var account = _currentAccountProvider.GetCurrentAccount(HttpContext.User);
+            var balance = _balanceFacade.GetBalance(account.Id);
+            return View("~/Views/Partial/_Balance.cshtml", balance);
+        }
+    }
+}
